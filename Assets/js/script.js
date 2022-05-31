be594878b8c53964807d2d8690c722b4255c9f16
valcomp <valerie.aleida.lopez@gmail.com>
2022-05-30 23:53:15 -0500

    created startup page

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+var startBtn = document.querySelector('#start_btn');
+
+var startTimer = function(event){
+    //event.preventDefault();
+    console.log("button was clicked");
+};
+
+startBtn.addEventListener('click',startTimer);
